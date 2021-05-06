eba14d364d15f9a36a53c1b76a39d955ee39cc41
Shoubhik <shoubhik2396@gmail.com>
2021-05-06 22:21:00 +0530

    minor changes

--- src/common/modal.js
+++ src/common/modal.js
@@ -3,7 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
-
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import { Box } from "@material-ui/core";
+import PhoneIcon from "@material-ui/icons/Phone";
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: "flex",
@@ -12,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
+
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -37,8 +39,26 @@ export default function TransitionsModal(props) {
       >
         <Fade in={props.open}>
           <div className={classes.paper}>
-            <h2 id="transition-modal-title">Reach us out at</h2>
-            <p id="transition-modal-description">shoubhik2396@gmail.com</p>
+            <h3
+              style={{ textAlign: "center", marginBottom: 20 }}
+              id="transition-modal-title"
+            >
+              Reach us out at
+            </h3>
+            <Box style={{ display: "flex", justifyContent: "space-between" }}>
+              <MailOutlineIcon
+                fontSize="small"
+                style={{ color: "rgb(232, 119, 22)", marginRight: 10 }}
+              ></MailOutlineIcon>
+              <p id="transition-modal-description">shoubhik2396@gmail.com</p>
+            </Box>
+            <Box style={{ display: "flex", justifyContent: "space-between" }}>
+              <PhoneIcon
+                style={{ color: "rgb(232, 119, 22)", marginRight: 10 }}
+                fontSize="small"
+              ></PhoneIcon>
+              <p id="transition-modal-description">shoubhik2396@gmail.com</p>
+            </Box>
           </div>
         </Fade>
       </Modal>
